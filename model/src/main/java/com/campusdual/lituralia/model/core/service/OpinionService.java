2ab51afeb10e06d2831ac0b4c7caf87db499b5e9
Lara Vázquez Dorna <lara.vazquez@campusdual.com>
2020-06-19 11:21:55 +0200

    LR-224 #OpinionService

--- model/src/main/java/com/campusdual/lituralia/model/core/service/OpinionService.java
+++ model/src/main/java/com/campusdual/lituralia/model/core/service/OpinionService.java
@@ -1,4 +1,44 @@
 package com.campusdual.lituralia.model.core.service;
 
-public class OpinionService {
+
+import com.campusdual.lituralia.api.core.service.IOpinionService;
+import com.campusdual.lituralia.model.core.dao.OpinionDao;
+import com.ontimize.db.EntityResult;
+import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service("OpinionService")
+@Lazy
+public class OpinionService implements IOpinionService {
+
+    @Autowired
+    private OpinionDao opinionDao;
+
+    @Autowired
+    private DefaultOntimizeDaoHelper daoHelper;
+
+    @Override
+    public EntityResult opinionQuery(Map<?, ?> keyMap, List<?> attrList) {
+        return this.daoHelper.query(opinionDao, keyMap, attrList);
+    }
+
+    @Override
+    public EntityResult opinionInsert(Map<?, ?> attrMap) {
+        return this.daoHelper.insert(opinionDao, attrMap);
+    }
+
+    @Override
+    public EntityResult opinionUpdate(Map<?, ?> attrMap, Map<?, ?> keyMap) {
+        return this.daoHelper.update(opinionDao, attrMap, keyMap);
+    }
+
+    @Override
+    public EntityResult opinionDelete(Map<?, ?> keyMap) {
+        return this.daoHelper.delete(opinionDao, keyMap);
+    }
 }
