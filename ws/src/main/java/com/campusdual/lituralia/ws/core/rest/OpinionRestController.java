a3e8bdc380d6e485844e0f1e490f9f644bd2d1c4
franciscosanchezt <francisco.sanchez@campusdual.com>
2020-06-19 17:26:03 +0200

    minor api route fix

--- ws/src/main/java/com/campusdual/lituralia/ws/core/rest/OpinionRestController.java
+++ ws/src/main/java/com/campusdual/lituralia/ws/core/rest/OpinionRestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/opinions")
+@RequestMapping("/api/opinions")
 @ComponentScan(basePackageClasses = {IOpinionService.class})
 public class OpinionRestController extends ORestController<IOpinionService> {
 
