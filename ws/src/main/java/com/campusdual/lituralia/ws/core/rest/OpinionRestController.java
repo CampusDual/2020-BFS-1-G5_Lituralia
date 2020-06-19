d42a25e291509cf587e3290582f98fef61628a90
Lara Vázquez Dorna <lara.vazquez@campusdual.com>
2020-06-19 11:32:39 +0200

    LR-225 #OpinionRestService

---
+++ ws/src/main/java/com/campusdual/lituralia/ws/core/rest/OpinionRestController.java
@@ -0,0 +1,22 @@
+package com.campusdual.lituralia.ws.core.rest;
+
+import com.campusdual.lituralia.api.core.service.IOpinionService;
+import com.ontimize.jee.server.rest.ORestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/opinions")
+@ComponentScan(basePackageClasses = {IOpinionService.class})
+public class OpinionRestController extends ORestController<IOpinionService> {
+
+    @Autowired
+    private IOpinionService opinionService;
+
+    @Override
+    public IOpinionService getService() {
+        return this.opinionService;
+    }
+}
