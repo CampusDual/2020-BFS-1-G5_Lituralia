9ee5cd7917d8211ba6b452f9c0b4462fdede3e9d
Lara Vázquez Dorna <lara.vazquez@campusdual.com>
2020-06-26 15:12:15 +0200

    add entitys booksgenre

--- api/src/main/java/com/campusdual/lituralia/api/core/service/IGenreService.java
+++ api/src/main/java/com/campusdual/lituralia/api/core/service/IGenreService.java
@@ -1,6 +1,8 @@
 package com.campusdual.lituralia.api.core.service;
 
 import com.ontimize.db.EntityResult;
+import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
+
 import java.util.List;
 import java.util.Map;
 
@@ -10,4 +12,14 @@ public interface IGenreService {
     EntityResult genreInsert(Map<?, ?> attrMap);
     EntityResult genreUpdate(Map<?, ?> attrMap, Map<?, ?> keyMap);
     EntityResult genreDelete(Map<?, ?> keyMap);
+    // ---- BOOK GENRES ----
+
+    EntityResult booksGenreQuery(Map<String, Object> keysValues, List<String> attributes) throws OntimizeJEERuntimeException;
+    EntityResult booksGenreInsert(Map<String, Object> attributes) throws OntimizeJEERuntimeException;
+    EntityResult booksGenreUpdate(Map<String, Object> attributes, Map<String, Object> KeyValues) throws OntimizeJEERuntimeException;
+    EntityResult booksGenreDelete(Map<String, Object> keyValues) throws OntimizeJEERuntimeException;
+
+    EntityResult vBooksGenreQuery(Map<String, Object> keysValues, List<String> attributes) throws OntimizeJEERuntimeException;
+    EntityResult vBooksGenreDelete(Map<String, Object> keyValues) throws OntimizeJEERuntimeException;
+
 }
--- model/src/main/java/com/campusdual/lituralia/model/core/service/GenreService.java
+++ model/src/main/java/com/campusdual/lituralia/model/core/service/GenreService.java
@@ -1,11 +1,15 @@
 package com.campusdual.lituralia.model.core.service;
 
 import com.campusdual.lituralia.api.core.service.IGenreService;
+import com.campusdual.lituralia.model.core.dao.BookGenreDao;
 import com.campusdual.lituralia.model.core.dao.GenreDao;
 import com.ontimize.db.EntityResult;
+import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
 import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
+
 import java.util.List;
 import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
@@ -17,6 +21,9 @@ public class GenreService implements IGenreService {
 
     @Autowired
     private GenreDao genreDao;
+
+    @Autowired
+    private BookGenreDao bookGenreDao;
 
     @Autowired
     private DefaultOntimizeDaoHelper daoHelper;
@@ -35,5 +42,43 @@ public class GenreService implements IGenreService {
 
     public EntityResult genreDelete(Map<?, ?> keyMap) {
         return this.daoHelper.delete(this.genreDao, keyMap);
+    }
+
+// ---- BOOKs GENRE ----
+
+    @Override
+    public EntityResult booksGenreQuery(Map<String, Object> keysValues, List<String> attributes)
+            throws OntimizeJEERuntimeException {
+        return this.daoHelper.query(this.bookGenreDao, keysValues, attributes);
+    }
+
+    @Override
+    public EntityResult booksGenreInsert(Map<String, Object> attributes) throws OntimizeJEERuntimeException {
+        return this.daoHelper.insert(this.bookGenreDao, attributes);
+    }
+
+    @Override
+    public EntityResult booksGenreUpdate(Map<String, Object> attributes, Map<String, Object> KeyValues)
+            throws OntimizeJEERuntimeException {
+        return this.daoHelper.update(this.bookGenreDao, attributes, attributes);
+    }
+
+    @Override
+    public EntityResult booksGenreDelete(Map<String, Object> keyValues) throws OntimizeJEERuntimeException {
+        return this.daoHelper.delete(this.bookGenreDao, keyValues);
+    }
+
+
+    // ---- V BOOK GENRE
+
+    @Override
+    public EntityResult vBooksGenreQuery(Map<String, Object> keysValues, List<String> attributes)
+            throws OntimizeJEERuntimeException {
+        return this.daoHelper.query(this.bookGenreDao, keysValues, attributes, BookGenreDao.QUERY_VBOOKGENRE);
+    }
+
+    @Override
+    public EntityResult vBooksGenreDelete(Map<String, Object> keyValues) throws OntimizeJEERuntimeException {
+        return this.daoHelper.delete(this.bookGenreDao, keyValues);
     }
 }
