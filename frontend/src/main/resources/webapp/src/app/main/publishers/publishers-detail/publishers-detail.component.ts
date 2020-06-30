877b22f394ce1eb7726040edd913276ca9856186
Nico <nicolas.fernandes@campusdual.com>
2020-06-30 17:44:38 +0200

    publishers detail

---
+++ frontend/src/main/resources/webapp/src/app/main/publishers/publishers-detail/publishers-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-publishers-detail',
+  templateUrl: './publishers-detail.component.html',
+  styleUrls: ['./publishers-detail.component.scss']
+})
+export class PublishersDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
