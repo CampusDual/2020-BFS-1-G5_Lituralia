477eef8614b832931c4c3bcf60e691e8d1d39ac7
franciscosanchezt <francisco.sanchez@campusdual.com>
2020-07-07 12:12:11 +0200

    SQL views optimizations

--- .docs/db/tables.sql
+++ .docs/db/tables.sql
@@ -285,6 +285,29 @@ create table lituralia.user_book_statuses
 );
 
 
+CREATE OR REPLACE VIEW lituralia.v_book_authors as
+(
+select b.book_id,
+       string_agg(cast(a.author_id as text), ',') as author_ids,
+       string_agg(a.author_name, ',')             as author_names
+from books b
+         LEFT JOIN book_authors ba on b.book_id = ba.book_id
+         LEFT JOIN authors a on ba.author_id = a.author_id
+GROUP BY b.book_id);
+
+
+
+CREATE OR REPLACE VIEW lituralia.v_book_genres as
+(
+select b.book_id,
+       string_agg(cast(g.genre_id as text), ',') as genre_ids,
+       string_agg(g.genre_name, ',')             as genre_names
+from books b
+         LEFT JOIN book_genres bg on b.book_id = bg.book_id
+         LEFT JOIN genres g on bg.genre_id = g.genre_id
+GROUP BY b.book_id);
+
+
 CREATE OR REPLACE VIEW lituralia.v_book_ratings AS
 (
 SELECT book_id,
@@ -295,7 +318,7 @@ GROUP BY book_id
     );
 
 
-CREATE OR REPLACE VIEW v_author_ratings AS
+CREATE OR REPLACE VIEW lituralia.v_author_ratings AS
 (
 SELECT ba.author_id,
        ROUND(AVG(rating), 2) avg_rating,
@@ -305,9 +328,18 @@ FROM opinions o
 GROUP BY ba.author_id
     );
 
+CREATE OR REPLACE VIEW lituralia.v_author_book_count AS
+(
+select authors.author_id,
+       count(*) books
+from authors
+         left outer join book_authors ba on authors.author_id = ba.author_id
+group by authors.author_id
+    );
 
-drop view if exists v_publisher_ratings;
-CREATE OR REPLACE VIEW v_publisher_ratings AS
+
+drop view if exists lituralia.v_publisher_ratings;
+CREATE OR REPLACE VIEW lituralia.v_publisher_ratings AS
 (
 SELECT b.publisher_id,
        ROUND(AVG(rating), 2) avg_rating,
@@ -315,61 +347,34 @@ SELECT b.publisher_id,
 FROM opinions o
          LEFT OUTER JOIN books AS b on o.book_id = b.book_id
 GROUP BY b.publisher_id
-);
+    );
 
 
 CREATE OR REPLACE VIEW lituralia.v_book_details AS
 (
 select b.*,
        p.publisher_name,
-       gn.genre_ids,
-       gn.genre_names,
-       an.author_ids,
-       an.author_names,
+       vbg.genre_ids,
+       vbg.genre_names,
+       vba.author_ids,
+       vba.author_names,
        v.avg_rating,
        v.ratings
 from books b
-         LEFT OUTER JOIN
-     v_book_ratings as v on v.book_id = b.book_id,
-     publishers p,
-     (select b.book_id,
-             string_agg(cast(a.author_id as text), ',') as author_ids,
-             string_agg(a.author_name, ',')             as author_names
-      from books b
-               LEFT JOIN book_authors ba
-                         on b.book_id = ba.book_id
-               LEFT JOIN authors a
-                         on ba.author_id = a.author_id
-      GROUP BY b.book_id) as an,
-     (select b.book_id,
-             string_agg(cast(g.genre_id as text), ',') as genre_ids,
-             string_agg(g.genre_name, ',')             as genre_names
-      from books b
-               LEFT JOIN book_genres bg
-                         on b.book_id = bg.book_id
-               LEFT JOIN genres g
-                         on bg.genre_id = g.genre_id
-      GROUP BY b.book_id) as gn
-where b.publisher_id = p.publisher_id
-  and b.book_id = an.book_id
-  and b.book_id = gn.book_id
+         LEFT OUTER JOIN v_book_ratings as v on v.book_id = b.book_id
+         LEFT OUTER JOIN publishers as p on b.publisher_id = p.publisher_id
+         LEFT OUTER JOIN v_book_genres vbg on b.book_id = vbg.book_id
+         LEFT OUTER JOIN v_book_authors vba on b.book_id = vba.book_id
     );
 
 
 CREATE OR REPLACE VIEW v_author_details AS
 (
 select a.*,
-       bc.books,
-       o.ratings,
-       o.avg_rating
-from authors a,
-     (select authors.author_id,
-             count(*) books
-      from authors
-               left outer join book_authors ba on authors.author_id = ba.author_id
-      group by authors.author_id) bc,
-     v_author_ratings o
-where a.author_id = bc.author_id
-  and a.author_id = o.author_id
-order by a.author_id
+       vabc.books,
+       var.ratings,
+       var.avg_rating
+from authors a
+         LEFT OUTER JOIN v_author_ratings var on a.author_id = var.author_id
+         LEFT OUTER JOIN v_author_book_count vabc on var.author_id = vabc.author_id
     );
