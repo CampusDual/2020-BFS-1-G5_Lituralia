9867fe110b029040c9d970e640dba3bedf47a1f7
Francisco Sanchez <frango9000@gmail.com>
2020-07-14 14:39:47 +0200

    real mock data  landing page   menu logo   log in register link in book detail

--- frontend/src/main/resources/webapp/src/app/app.component.ts
+++ frontend/src/main/resources/webapp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {LoginService} from "ontimize-web-ngx";
 
 @Component({
     selector: 'o-app',
@@ -7,7 +8,7 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-    constructor() {
+    constructor(public loginService: LoginService) {
     }
 
     ngOnInit() {
--- frontend/src/main/resources/webapp/src/app/shared/app.menu.config.ts
+++ frontend/src/main/resources/webapp/src/app/shared/app.menu.config.ts
@@ -6,14 +6,10 @@ export const MENU_CONFIG: MenuRootItem[] = [
   {id: 'authors', name: 'AUTHORS', icon: 'people', route: '/main/authors'},
   {id: 'genres', name: 'GENRES', icon: 'style', route: '/main/genres'},
   {id: 'publishers', name: 'PUBLISHERS', icon: 'business', route: '/main/publishers'},
-  {id: 'user', name: 'USER_MENU', icon: 'account_circle', opened: false,
-    items: [
-      { id: 'profile', name: 'PROFILE', route: '/main/user/profile', icon: 'perm_contact_calendar' },
-      { id: 'timeline', name: 'TIMELINE', route: '/main/user/timeline', icon: 'timeline' },
-      { id: 'mylist', name: 'MY_LIST', route: '/main/user/mylist', icon: 'favorites' }
-    ]
-  },
-  {id: 'lang_opts', name: 'LANGUAGES', icon: 'language', items: [
+  {id: 'timeline', name: 'TIMELINE', route: '/main/user/timeline', icon: 'timeline'},
+  {id: 'mylist', name: 'MY_LIST', route: '/main/user/mylist', icon: 'favorites'},
+  {
+    id: 'lang_opts', name: 'LANGUAGES', icon: 'language', items: [
       {id: 'lang_es', name: 'LOCALE_es', icon: 'language', locale: 'es'},
       {id: 'lang_en', name: 'LOCALE_en', icon: 'language', locale: 'en'}
     ]
